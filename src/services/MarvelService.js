400b8191bb6e43ccd754e65521ad00c529f3742a
Anastasiia <bardieranastasi@gmail.com>
2023-12-18 17:50:16 +0200

    додала методи по отриманню персонажа по id та отримання групи персонажів

---
+++ src/services/MarvelService.js
@@ -0,0 +1,48 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=e71e2718fabff072296a2d9a69c1759c";
+
+  //функція getResource - запитує дані через fetch, чекає на відповідь у разі помилки(серверні, наприклад 404) видає повідомлення у консоль, якщо все ок, то отримуємо відповідь у форматі json
+  getResource = async (url) => {
+    let result = await fetch(url);
+
+    if (!result.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${result.status}`);
+    }
+
+    return await result.json();
+  };
+
+  // запити до API
+
+  //запит на групу персонажів
+  getAllCharacters = async () => {
+    const result = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`,
+    ); // Request URL з сайту Marvel Developer
+
+    //повертаємо масив з новими об'єктами
+    return result.data.results.map(this._transformCharacter);
+  };
+
+  //запит на одного персонажа
+  getCharacter = async (id) => {
+    const result = await this.getResource(
+      `${this._apiBase}characters/${id}?&${this._apiKey}`,
+    ); // Request URL з сайту Marvel Developer
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  //метод отримує дані та трансформує в необхідні. в нашому випадку отримує великий об'єкт з даними про персонажа і повертає об'єкт з потрібними нам даними
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
+}
+
+export default MarvelService;
